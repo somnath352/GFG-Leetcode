c2ec8d7cfdec441ca8987e4d7327acde18e7f535
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2022-10-13 20:11:47 +0530

    Time: 4 ms (26.79%), Space: 5.9 MB (26.50%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
+        int cnt = 0;
         int _xor = start ^ goal;
-        int cntMinBits = __builtin_popcount(_xor);
-        return cntMinBits;
+        // int cntMinBits = __builtin_popcount(_xor);
+        for(int i = 0;i<32;i++) {
+            if(_xor&(1<<i)) cnt++;
+        }
+        return cnt;
     }
 };
