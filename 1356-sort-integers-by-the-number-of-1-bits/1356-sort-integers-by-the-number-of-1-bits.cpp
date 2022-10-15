26064ef6f392d0a515c716535aee7fdfaa6658d0
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2022-10-15 15:55:44 +0530

    Time: 7 ms (96.02%), Space: 10.1 MB (88.50%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,15 +1,10 @@
 class Solution {
-private:
-    static bool comp(int &a, int &b) {
-        int cntA = bitset<32>(a).count();
-        int cntB = bitset<32>(b).count();
-        if(cntA == cntB) return a < b;
-        return cntA < cntB;
-        
-    }
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), comp);
+        sort(arr.begin(), arr.end(), [&](int &a, int &b) {
+            int cntA = __builtin_popcount(a), cntB = __builtin_popcount(b);
+            return cntA == cntB ? a < b : cntA < cntB;
+        });
         return arr;
     }
 };
