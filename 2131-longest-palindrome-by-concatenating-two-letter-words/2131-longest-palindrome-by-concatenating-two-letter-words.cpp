69261d0e14fd7526f3152c8bd5f1cf0ddafe8ee4
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2022-11-04 00:16:54 +0530

    Time: 625 ms (72.40%), Space: 168.4 MB (20.85%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string, int> mpp;
-        int longestPalindrome = 0, unpair = 0;
-        for(auto ele : words) {
-            if(ele[0] == ele[1]) {
-                if(mpp[ele]) {
-                    longestPalindrome += 4;
-                    mpp[ele]--;
-                    unpair--;
-                }
-                else {
-                    mpp[ele]++;
-                    unpair++;
-                }
+        vector<vector<int>> counter(26, vector<int>(26, 0));
+        int longestPalindrome = 0;
+        
+        for(string ele : words) {
+            int first = ele[0] - 'a', second = ele[1] - 'a';
+            if(counter[second][first]) {
+                longestPalindrome += 4;
+                counter[second][first]--;
             }
-            
-            else {
-                string rev = ele;
-                reverse(rev.begin(), rev.end());
-                if(mpp[rev]) {
-                    longestPalindrome += 4;
-                    mpp[rev]--;
-                }
-                else {
-                    mpp[ele]++;
-                }
+            else counter[first][second]++;
+        }
+        for(int i = 0;i<26;i++) {
+            if(counter[i][i]) {
+                longestPalindrome += 2;
+                break;
             }
         }
-        if(unpair > 0) longestPalindrome += 2;
         return longestPalindrome;
     }
 };
